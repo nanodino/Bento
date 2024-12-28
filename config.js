f37203561d9b208c7e8c0e5a65f825de73cc9f7d
nancy <ncantu@fastmail.com>
2024-12-28 09:33:10 -0500

    modify light theme css

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: '85b8c87b4b7d20e0c1ea58fdec87cde2', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
